cf5026797a70a928aaef50064a15b8304414515c
Jonesy22 <Jonesy22@users.noreply.github.com>
2020-09-03 12:16:37 -0700

    Finished each selection. Need error handling

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class State():
 		self.currentICU = currentICU
 		self.totalICU = totalICU
 		self.recovered = recovered
-		self.dateGrade = dataGrade
+		self.dataGrade = dataGrade
 		self.deaths = deaths
 		self.totalTests = totalTests
 
@@ -98,10 +98,8 @@ def userMenu():
 		print("5) Current in ICU (All states)")
 		print("6) Total in ICU (All states)")
 		print("7) Total Recovered (All states)")
-		print("8) Total Hospitalized (All states)")
-		print("9) Total Deaths (All states)")
-		print("10) Total Deaths (All states)")
-		print("11) Choose a state to analyze")
+		print("8) Total Deaths (All states)")
+		print("9) Choose a state to analyze")
 		print("0) Exit the analyzer")
 		choice = input("--> ")
 		return choice
@@ -111,11 +109,15 @@ def displayTotalCases(statesList):
 	subj = "\n---Total Cases by state---\n"
 	print(subj)
 	for s in statesList:
+		if s.positiveCases < '1':
+			s.positiveCases = 'N/A'
 		print(s.state + ": "  + s.positiveCases + " cases as of " + s.date + '\n')
 	yesOrNo = input("\nWould you like to receive an email with this information? (Y/N): ").lower()
 	if yesOrNo == "y":
 		message = ''
 		for s in statesList:
+			if s.positiveCases < '1':
+				s.positiveCases = 'N/A'
 			message += (s.state + ": " + s.positiveCases + " cases as of " + s.date + '\n')
 		sendEmail(statesList, message, subj)
  
@@ -123,13 +125,155 @@ def displayNegTests(statesList):
 	subj = "\n---Negative Tests by state---\n"
 	print(subj)
 	for s in statesList:
+		if s.negativeCases < '1':
+			s.negativeCases = 'N/A'
 		print(s.state + ": "  + s.negativeCases)
 	yesOrNo = input("\nWould you like to receive an email with this information? (Y/N): ").lower()
 	if yesOrNo == "y":
 		message = ''
 		for s in statesList:
+			if s.negativeCases < '1':
+				s.negativeCases = 'N/A'
 			message += (s.state + ": " + s.negativeCases + " negative tests as of " + s.date + '\n')
 		sendEmail(statesList, message, subj)
+
+def currentlyHospitalized(statesList):
+	subj = "\n---Currently Hospitalized---\n"
+	print(subj)
+	for s in statesList:
+		if s.currentHosp < '1':
+			s.currentHosp = 'N/A'
+		print(s.state + ": "  + s.currentHosp)
+	yesOrNo = input("\nWould you like to receive an email with this information? (Y/N): ").lower()
+	if yesOrNo == "y":
+		message = ''
+		for s in statesList:
+			if s.currentHosp < '1':
+				s.currentHosp = 'N/A'
+			message += (s.state + ": " + s.currentHosp + " currently hospitalized as of " + s.date + '\n')
+		sendEmail(statesList, message, subj)
+
+def totalHospitalized(statesList):
+	subj = "\n---Total Hospitalized---\n"
+	print(subj)
+	for s in statesList:
+		if s.totalHosp < '1':
+			s.totalHosp = 'N/A'
+		print(s.state + ": "  + s.totalHosp)
+	yesOrNo = input("\nWould you like to receive an email with this information? (Y/N): ").lower()
+	if yesOrNo == "y":
+		message = ''
+		for s in statesList:
+			if s.totalHosp < '1':
+				s.totalHosp = 'N/A'
+			message += (s.state + ": " + s.totalHosp + " total hospitalized as of " + s.date + '\n')
+		sendEmail(statesList, message, subj)
+
+def currentICU(statesList):
+	subj = "\n---Currently in ICU---\n"
+	print(subj)
+	for s in statesList:
+		if s.currentICU < '1':
+			s.currentICU = 'N/A'
+		print(s.state + ": "  + s.currentICU)
+	yesOrNo = input("\nWould you like to receive an email with this information? (Y/N): ").lower()
+	if yesOrNo == "y":
+		message = ''
+		for s in statesList:
+			if s.currentICU < '1':
+				s.currentICU = 'N/A'
+			message += (s.state + ": " + s.currentICU + " currently in ICU as of " + s.date + '\n')
+		sendEmail(statesList, message, subj)
+
+def totalICU(statesList):
+	subj = "\n---Total in ICU---\n"
+	print(subj)
+	for s in statesList:
+		if s.totalICU < '1':
+			s.totalICU = 'N/A'
+		print(s.state + ": "  + s.totalICU)
+	yesOrNo = input("\nWould you like to receive an email with this information? (Y/N): ").lower()
+	if yesOrNo == "y":
+		message = ''
+		for s in statesList:
+			if s.totalICU < '1':
+				s.totalICU = 'N/A'
+			message += (s.state + ": " + s.totalICU + " total in ICU as of " + s.date + '\n')
+		sendEmail(statesList, message, subj)
+
+def totalRecovered(statesList):
+	subj = "\n---Total Recovered---\n"
+	print(subj)
+	for s in statesList:
+		if s.reocvered < '1':
+			s.recovered = 'N/A'
+		print(s.state + ": "  + s.recovered)
+	yesOrNo = input("\nWould you like to receive an email with this information? (Y/N): ").lower()
+	if yesOrNo == "y":
+		message = ''
+		for s in statesList:
+			if s.recovered < '1':
+				s.recovered = 'N/A'
+			message += (s.state + ": " + s.recovered + " total in ICU as of " + s.date + '\n')
+		sendEmail(statesList, message, subj)
+
+def totalDeaths(statesList):
+	subj = "\n---Total Deaths---\n"
+	print(subj)
+	for s in statesList:
+		if s.deaths < '1':
+			s.deaths = 'N/A'
+		print(s.state + ": "  + s.deaths)
+	yesOrNo = input("\nWould you like to receive an email with this information? (Y/N): ").lower()
+	if yesOrNo == "y":
+		message = ''
+		for s in statesList:
+			if s.deaths < '1':
+				s.deaths = 'N/A'
+			message += (s.state + ": " + s.deaths + " total in ICU as of " + s.date + '\n')
+		sendEmail(statesList, message, subj)
+
+def analyzeState(statesList):
+	stateChosen = input("Which state would you like to see the stats of? (EX: WA/wa/Wa/wA, not case sensitive): ").upper()
+	print("\n---Statistics for the state of " + stateChosen + "---\n")
+	for s in statesList:
+		if stateChosen == s.state:
+			if s.positiveCases < '1': s.positiveCases = "N/A"
+			if s.negativeCases < '1': s.negativeCases = "N/A"
+			if s.currentHosp < '1': s.currentHosp = "N/A"
+			if s.totalHosp < '1': s.totalHosp = "N/A"
+			if s.currentICU < '1': s.currentICU = "N/A"
+			if s.totalICU < '1': s.totalICU = "N/A"
+			if s.recovered < '1': s.recovered = "N/A"
+			if s.deaths < '1': s.deaths = "N/A"
+			print("Total positive cases: " + s.positiveCases)
+			print("Negative tests: " + s.negativeCases)
+			print("Currently in hospital: " + s.currentHosp)
+			print("Total hospitalized: " + s.totalHosp)
+			print("Currently in ICU: " + s.currentICU)
+			print("Total in ICU: " + s.totalICU)
+			print("Total recovered: " + s.recovered)
+			print("Total deaths: " + s.deaths)
+			print("Data quality/Reliability grade: " + s.dataGrade + "\n")
+	yesOrNo = input("\nWould you like to receive an email with this information? (Y/N): ").lower()
+	if yesOrNo == "y":
+		for s in statesList:
+			if stateChosen == s.state:
+				if s.positiveCases < '1': s.positiveCases = "N/A"
+				if s.negativeCases < '1': s.negativeCases = "N/A"
+				if s.currentHosp < '1': s.currentHosp = "N/A"
+				if s.totalHosp < '1': s.totalHosp = "N/A"
+				if s.currentICU < '1': s.currentICU = "N/A"
+				if s.totalICU < '1': s.totalICU = "N/A"
+				if s.recovered < '1': s.recovered = "N/A"
+				if s.deaths < '1': s.deaths = "N/A"
+				subj = ''
+				message = ''
+				message += ("---Statistics for the state of " + stateChosen + "---" + "\nTotal positive cases: " + s.positiveCases + "\nNegative tests: " + s.negativeCases + "\nCurrently in hospital: " + s.currentHosp + 
+				"\nTotal hospitalized: " + s.totalHosp + "\nCurrently in ICU: " + s.currentICU + "\nTotal in ICU: " + s.totalICU + "\nTotal recovered: " + s.recovered + 
+				"\nTotal deaths: " + s.deaths + "\nData quality/Reliability grade: " + s.dataGrade + "\n")
+				sendEmail(statesList, message, subj)
+
 
 # Sends an email to the user with the email they entered as well as what they want emailed to them
 # Need to figure out a way to have email hardcoded, but not in plaintext so users can see
@@ -145,7 +289,7 @@ def sendEmail(statesList, message, subj):
 	msg['To'] = receiver_email
 	msg['Subject'] = "COVID-19 Analyzer Results"
 	msg.attach(MIMEText(message, 'plain'))
-	s.send_message(msg)
+	sendSuccessful = s.send_message(msg)
 	del msg
 
 # This is where the API gets called originally, and the other functions are called to get things done
@@ -165,19 +309,19 @@ def main():
 		elif choice == '2':
 			displayNegTests(statesList)
 		elif choice == '3':
-			print("Choice 2 chosen")
+			currentlyHospitalized(statesList)
 		elif choice == '4':
-			print("Choice 2 chosen")
+			totalHospitalized(statesList)
 		elif choice == '5':
-			print("Choice 2 chosen")
+			currentICU(statesList)
 		elif choice == '6':
-			print("Choice 2 chosen")
+			totalICU(statesList)
 		elif choice == '7':
-			print("Choice 2 chosen")
+			totalRecovered(statesList)
 		elif choice == '8':
-			print("Choice 2 chosen")
+			totalDeaths(statesList)
 		elif choice == '9':
-			print("Choice 2 chosen")
+			analyzeState(statesList)
 		elif choice == '11':
 			print("Choice 2 chosen")
 	print("Thank you for using the Covid-Analyzer!")
